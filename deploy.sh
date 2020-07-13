aea7608e7e39cf0ed1879cd1f3e8e2072c9a39cd
Aayush Tuladhar <aayush.tuladhar@gmail.com>
2020-07-13 14:06:29 -0500

    Adding Deploy to GitHub Page Script

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+if [[ $(git status -s) ]]
+then
+    echo "The working directory is dirty. Please commit any pending changes."
+    exit 1;
+fi
+
+echo "Deleting Old Publications"
+rm -rf public && mkdir public
+git worktree prune
+rm -rf .git/worktrees/public/
+
+echo "Checking out gh-pages into public"
+git worktree add -B gh-pages public origin/gh-pages
+cd public && git submodule update
+mkdir -p content/AWS && mv *.md content/AWS/.
+rm README.md
+
+echo "Generating Site"
+hugo -b "http://www.aayushtuladhar.com/aws-solutions-architect-associate-notes/" -d .
+
+
+echo "Updating gh-pages branch"
+git add --all && git commit -m "Publishing to gh-pages - `date`"
+
+git push --set-upstream origin gh-pages
+cd ..
+rm -rf public && git worktree prune
